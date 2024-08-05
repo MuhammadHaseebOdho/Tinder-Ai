aeab12a48b50a8f5da13119b73431f5d47ce97a3
MuhammadHaseebOdho <haseeb.odho03@gmail.com>
2024-08-05 15:56:38 +0500

    Created Project

---
+++ Tinder Ai Project/src/main/java/com/tinder/TinderAiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinderAiProjectApplication.class, args);
+    }
+
+}
